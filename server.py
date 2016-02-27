9ddaee0d49ace58476fc35142a0eeb4b97d39ccf
Ben Doan <ben@bendoan.me>
2016-02-27 10:40:41 -0600

    added device control and indicators on frontend

--- server.py
+++ server.py
@@ -3,9 +3,15 @@ from flask import *
 
 app = Flask(__name__)
 
+light_states = {
+    "projector": True,
+    "light1": False,
+    "light2": False
+}
+
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", **light_states)
 
 @app.route('/js/<remainder>',methods=['GET'])
 @app.route('/img/<remainder>',methods=['GET'])
@@ -14,4 +20,4 @@ def get_static(remainder):
     return send_from_directory(app.static_folder,request.path[1:])
 
 if __name__ == "__main__":
-    app.run(host="0.0.0")
+    app.run(host="0.0.0", debug=True)
